bb5a975d6777796613bb59f7952d93a7713e6605
Komal Joshi <moudgillkomal6@gmail.com>
2026-01-24 17:47:46 +0530

    new file:   DSA/TWO POINTER/06_remove_duplicates.cpp

---
+++ DSA/TWO POINTER/06_remove_duplicates.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         int n=1;
+//         for(int i=1;i<nums.size();i++){
+//             if(nums[i]!=nums[i-1]){
+//                 nums[n]=nums[i];
+//                 n++;
+//             } 
+//         }
+//         return n;
+//     }
+// };
