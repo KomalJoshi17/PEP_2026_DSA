4afe1b42c3f3b8efffc2bd854846867ff8445f8a
Komal Joshi <moudgillkomal6@gmail.com>
2026-01-24 09:41:42 +0530

    modified:   DSA/TIME & SPACE/01_time.cpp 	new file:   DSA/TIME & SPACE/02_space.cpp

--- DSA/TIME & SPACE/01_time.cpp
+++ DSA/TIME & SPACE/01_time.cpp
@@ -1,1 +1,32 @@
-// time complexity -> how much time an alogrithm takes as input size increases
+/* time complexity -> how much time an alogrithm takes as input size increases
+
+bingo -> worse case time complexity, represents the upper bound of running time, it gives the worse-case scenario for an algorithm, it describes the maximum time an algorithm can take.
+
+theta and omega 
+
+1.
+O(1) -> constant time -> time taken doesn't depend on the input size, the algorithm always runs in the same time
+example 1: cout<<"hi"; one time it will run
+example 2: for(int i=0;i<10;i++){ } 10 times it will run
+
+2.
+O(log n) -> logarithmic time -> time grows logarithmically as the input increases, often seen in algorithms that divide the input into half
+example 1: binary search 
+
+3.
+O(n) -> linear time -> time grows directly proportional to input size
+example 1: loop running for n times, as n increases, time increase
+for(int i=0;i<n;i++){ }
+
+4. 
+O(n log n) -> linearithmic time -> a mix of linear and logarithmic
+example 1: divide and conquor (quick and merge sort)
+
+5.
+O(n^2) -> quadratic time -> time grows proportionally to the square of input size
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;i++){
+        
+    }
+}
+*/    
---
+++ DSA/TIME & SPACE/02_space.cpp
@@ -0,0 +1,6 @@
+/* space complexity -> how much extra space you are uitlising to perform the particular algorithm
+
+two types -> constant space / n space 
+O(1) -> no extra space
+O(n) -> formation of any array or any other thing to save data inside it or perform operations
+*/
