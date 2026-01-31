e42d08e3a2a2ec300e5e41085cade61cba7f9803
Komal Joshi <moudgillkomal6@gmail.com>
2026-01-31 13:45:30 +0530

    save

---
+++ DSA/2D_ARRAYS/02_searching.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+bool found(int arr[4][4],int key){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(arr[i][j]==key){
+                cout<<i<<" "<<j<<" at indices"<<endl;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+    int arr[4][4]={
+        {1,7,14,6},
+        {11,15,5,10},
+        {2,4,16,3},
+        {9,13,8,12}
+    };
+
+    int key=9;
+
+    cout<<found(arr,key);
+    return 0;
+}
