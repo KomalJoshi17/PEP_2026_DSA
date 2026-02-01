ca441bb366432354a873f371cf8472a92627b822
Komal Joshi <moudgillkomal6@gmail.com>
2026-02-01 19:56:24 +0530

    new file:   DSA/SHEET/A_print_recursion.cpp

---
+++ DSA/SHEET/A_print_recursion.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void print(int n){
+    if(n==0) return;
+
+    cout<<"I love Recursion"<<endl;
+    print(n-1);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    
+    print(n);
+    return 0;
+}
