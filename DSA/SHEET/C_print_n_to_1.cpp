1c794e8c667d7731244092faa6be0b6b6edeac8e
Komal Joshi <moudgillkomal6@gmail.com>
2026-02-01 19:57:50 +0530

    new file:   DSA/SHEET/C_print_n_to_1.cpp

---
+++ DSA/SHEET/C_print_n_to_1.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/C
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void print(int n){
+    if(n==0) return;
+
+    cout<<n;
+    if(n>1) cout<<" ";
+    print(n-1);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
