d8ff9dd9096ef51a4ccd99b40dc53e55248d0690
Komal Joshi <moudgillkomal6@gmail.com>
2026-02-04 12:30:16 +0530

    new file:   PRACTICE/Height_Mismatch_Counter.cpp

---
+++ PRACTICE/Height_Mismatch_Counter.cpp
@@ -0,0 +1,12 @@
+// int solve(const vector<int>& nums) {
+//     int n = nums.size();
+//     vector<int> expected = nums; 
+//     sort(expected.begin(), expected.end());
+//     int count = 0;
+//     for(int i = 0; i < n; i++){
+//         if(expected[i] != nums[i]){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
