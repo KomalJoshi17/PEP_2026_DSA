6ae545e290dcc41cab156b64dfdebfc2374c9b35
Komal Joshi <moudgillkomal6@gmail.com>
2026-02-06 08:59:35 +0530

    save

--- DSA/ARRAYS/10_third_largest.cpp
+++ DSA/ARRAYS/10_third_largest.cpp
@@ -1,2 +1,12 @@
 // https://www.geeksforgeeks.org/problems/third-largest-element/1
 
+// class Solution {
+//   public:
+//     int thirdLargest(vector<int> &arr) {
+        // code here
+        // if(arr.size()<=2) return -1;
+        // sort(arr.begin(),arr.end());
+        
+        // return arr[arr.size()-3];
+//     }
+// };
