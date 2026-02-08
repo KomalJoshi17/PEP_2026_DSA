6f4ac9c0056ec6fe6914e91eab4d29a2cc92ae73
Komal Joshi <moudgillkomal6@gmail.com>
2026-02-08 17:53:09 +0530

    save

--- PRACTICE/Magical_Square.cpp
+++ PRACTICE/Magical_Square.cpp
@@ -1,33 +1,33 @@
-long long countPerfectSquaresInRange(long long L, long long R) {
-    // add your code here
-    long long low=0;
-    long long high=1e9;
-    long long first=-1;
-    long long last=-1;
+// long long countPerfectSquaresInRange(long long L, long long R) {
+//     // add your code here
+//     long long low=0;
+//     long long high=1e9;
+//     long long first=-1;
+//     long long last=-1;
 
-    while(low<=high){
-        long long mid=low+(high-low)/2;
-        if(mid*mid>=L){
-            first=mid;
-            high=mid-1;
-        }else{
-            low=mid+1;
-        }
-    }
+//     while(low<=high){
+//         long long mid=low+(high-low)/2;
+//         if(mid*mid>=L){
+//             first=mid;
+//             high=mid-1;
+//         }else{
+//             low=mid+1;
+//         }
+//     }
 
-    low=0;
-    high=1e9;
+//     low=0;
+//     high=1e9;
 
-    while(low<=high){
-        long long mid=low+(high-low)/2;
-        if(mid*mid<=R){
-            last=mid;
-            low=mid+1;
-        }else{
-            high=mid-1;
-        }
-    }
+//     while(low<=high){
+//         long long mid=low+(high-low)/2;
+//         if(mid*mid<=R){
+//             last=mid;
+//             low=mid+1;
+//         }else{
+//             high=mid-1;
+//         }
+//     }
 
-    if(first>last || first==-1 || last==-1) return 0;
-    return last-first+1;
-}
+//     if(first>last || first==-1 || last==-1) return 0;
+//     return last-first+1;
+// }
