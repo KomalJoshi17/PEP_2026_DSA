c3968970df37399e3fe426a207a868501513ffd1
Komal Joshi <moudgillkomal6@gmail.com>
2026-02-08 17:53:24 +0530

    save

--- PRACTICE/Count_Unique_Artifacts.cpp
+++ PRACTICE/Count_Unique_Artifacts.cpp
@@ -1,14 +1,14 @@
-int countDistinctArtifacts(vector<int>& ids) {
-    // add your code here
-    if(ids.empty()) return 0;
+// int countDistinctArtifacts(vector<int>& ids) {
+//     // add your code here
+//     if(ids.empty()) return 0;
 
-    sort(ids.begin(),ids.end());
+//     sort(ids.begin(),ids.end());
 
-    int count=1;
-    for(int i=1;i<ids.size();i++){
-        if(ids[i]!=ids[i-1]){
-            count++;
-        }
-    }
-    return count;
-}
+//     int count=1;
+//     for(int i=1;i<ids.size();i++){
+//         if(ids[i]!=ids[i-1]){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
