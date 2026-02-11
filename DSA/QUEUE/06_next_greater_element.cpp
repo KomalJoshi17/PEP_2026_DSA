a8fa5d90992456ffd202d09eab20ad10cb7b4285
Komal Joshi <moudgillkomal6@gmail.com>
2026-02-11 10:42:35 +0530

    save

---
+++ DSA/QUEUE/06_next_greater_element.cpp
@@ -0,0 +1,26 @@
+// https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+
+// class Solution {
+//   public:
+//     vector<int> nextLargerElement(vector<int>& arr) {
+//         // code here
+//         stack<int>st;
+//         int n=arr.size();
+//         vector<int>ans(n);
+        
+//         for(int i=n-1;i>=0;i--){
+//             // remove all elements smaller than or equal to current element
+//             while(!st.empty() && st.top()<=arr[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()){
+//                 ans[i]=-1;
+//             }
+//             else{
+//                 ans[i]=st.top();
+//             }
+//             st.push(arr[i]);
+//         }
+//         return ans;
+//     }
+// };
