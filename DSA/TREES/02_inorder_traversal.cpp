d51b624f58645b93a9042153fa862fa77e7c618d
Komal Joshi <moudgillkomal6@gmail.com>
2026-02-13 15:23:49 +0530

    save

--- DSA/TREES/02_inorder_traversal.cpp
+++ DSA/TREES/02_inorder_traversal.cpp
@@ -1,8 +1,34 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
+// https://www.geeksforgeeks.org/problems/inorder-traversal/1
+
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+// class Solution {
+//   public:
+//     void inorder(Node* root, vector<int>&ans){
+//         if(root==NULL) return;
+        
+//         inorder(root->left,ans);
+//         ans.push_back(root->data);
+//         inorder(root->right,ans);
+//     }
     
-int main(){
-    
-    return 0;
-}
+//     vector<int> inOrder(Node* root) {
+//         // code here
+//         vector<int>ans;
+//         inorder(root,ans);
+//         return ans;
+//     }
+// };
