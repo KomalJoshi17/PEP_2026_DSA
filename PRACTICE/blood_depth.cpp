2a382c2ae81bf2e536f9d6ab104f49a8e0a82697
Komal Joshi <moudgillkomal6@gmail.com>
2026-02-28 23:28:18 +0530

    save

--- PRACTICE/blood_depth.cpp
+++ PRACTICE/blood_depth.cpp
@@ -1,32 +1,32 @@
-/* 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-*/
+// /* 
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+// };
+// */
 
-int deepestLeavesSum(TreeNode* root) {
-    // add your code here
-    if(!root) return 0;
+// int deepestLeavesSum(TreeNode* root) {
+//     // add your code here
+//     if(!root) return 0;
 
-    queue<TreeNode*>q;
-    q.push(root);
-    int sum=0;
-    while(!q.empty()){
-        int size=q.size();
-        sum=0;
+//     queue<TreeNode*>q;
+//     q.push(root);
+//     int sum=0;
+//     while(!q.empty()){
+//         int size=q.size();
+//         sum=0;
 
-        for(int i=0;i<size;i++){
-            TreeNode* node=q.front();
-            q.pop();
+//         for(int i=0;i<size;i++){
+//             TreeNode* node=q.front();
+//             q.pop();
 
-            sum+=node->val;
+//             sum+=node->val;
 
-            if(node->left) q.push(node->left);
-            if(node->right) q.push(node->right);
-        }
-    }
-    return sum;
-}
+//             if(node->left) q.push(node->left);
+//             if(node->right) q.push(node->right);
+//         }
+//     }
+//     return sum;
+// }
