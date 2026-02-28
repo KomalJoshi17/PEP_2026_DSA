46af78879dfe51e9be33593fa6d390506f75f5e5
Komal Joshi <moudgillkomal6@gmail.com>
2026-02-28 23:29:51 +0530

    save

--- PRACTICE/diagonal_wave_collector.cpp
+++ PRACTICE/diagonal_wave_collector.cpp
@@ -1,19 +1,19 @@
-// vector<vector<int>> diagonalWaveCollector(vector<vector<int>>& grid) {
-//     // add your code here
-//     int m=grid.size();
-//     int n=grid[0].size();
-//     vector<vector<int>>result;
+vector<vector<int>> diagonalWaveCollector(vector<vector<int>>& grid) {
+    // add your code here
+    int m=grid.size();
+    int n=grid[0].size();
+    vector<vector<int>>result;
 
-//     for(int i=0;i<=m+n-2;i++){
-//         vector<int>diagonal;
-//         for(int j=0;j<m;j++){
-//             int k=i-j;
+    for(int i=0;i<=m+n-2;i++){
+        vector<int>diagonal;
+        for(int j=0;j<m;j++){
+            int k=i-j;
 
-//             if(k>=0 && k<n){
-//                 diagonal.push_back(grid[j][k]);
-//             }
-//         }
-//         result.push_back(diagonal);
-//     }
-//     return result;
-// }
+            if(k>=0 && k<n){
+                diagonal.push_back(grid[j][k]);
+            }
+        }
+        result.push_back(diagonal);
+    }
+    return result;
+}
