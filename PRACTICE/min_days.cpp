25b84ce9e18d83100e99928bb8800fb8da7a2505
Komal Joshi <moudgillkomal6@gmail.com>
2026-03-02 14:38:52 +0530

    save

---
+++ PRACTICE/min_days.cpp
@@ -0,0 +1,19 @@
+int minSubArrayLen(int target, vector<int>& nums) {
+    // add your code here    
+    int i=0;
+    int sum=0;
+    int len=INT_MAX;
+    int n=nums.size();
+
+    for(int j=0;j<n;j++){
+        sum+=nums[j];
+
+        while(sum>=target){
+            len=min(len,j-i+1);
+            sum-=nums[i];
+            i++;
+        }
+    }
+
+    return (len==INT_MAX)?0:len;
+}
