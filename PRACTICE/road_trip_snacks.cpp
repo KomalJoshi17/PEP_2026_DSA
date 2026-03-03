79835e32ff6020168193e2db99930d42a64ce465
Komal Joshi <moudgillkomal6@gmail.com>
2026-03-03 10:28:32 +0530

    save

---
+++ PRACTICE/road_trip_snacks.cpp
@@ -0,0 +1,15 @@
+int maxEnergyBars(vector<int>& arr, int k) {
+    // Your implementation here
+    int sum=0;
+    for(int i=0;i<k;i++){
+        sum+=arr[i];
+    }
+    int maxSum=sum;
+    for(int i=k;i<arr.size();i++){
+        sum=sum+arr[i]-arr[i-k];
+        if(sum>maxSum){
+            maxSum=sum;
+        }
+    }
+    return maxSum;
+}
