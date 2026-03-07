1a55d6e4fa625f79c8cabb00f30f4099914865e2
Komal Joshi <moudgillkomal6@gmail.com>
2026-03-07 11:17:16 +0530

    save

--- PRACTICE/print_linked_list.cpp
+++ PRACTICE/print_linked_list.cpp
@@ -1,17 +1,4 @@
-/*
-class Node {
-  public:
-    int data;
-    Node *next;
-    Node(int x) {
-        data = x;
-        next = nullptr;
-    }
-};
-*/
-
 void printList(Node* head) {
-    // add your code here
     Node* temp=head;
     while(temp!=nullptr){
         cout<<temp->data;
