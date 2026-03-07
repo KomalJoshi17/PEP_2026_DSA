e3f1daa15a441675c9a455b66c0b4f4e5958a9c3
Komal Joshi <moudgillkomal6@gmail.com>
2026-03-07 11:15:52 +0530

    save

--- PRACTICE/librarian_count.cpp
+++ PRACTICE/librarian_count.cpp
@@ -1,49 +1,49 @@
-// int firstOccurrence(vector<int>& arr, int target) {
-//     int low = 0, high = arr.size() - 1;
-//     int ans = -1;
+int firstOccurrence(vector<int>& arr, int target) {
+    int low = 0, high = arr.size() - 1;
+    int ans = -1;
 
-//     while (low <= high) {
-//         int mid = low + (high - low) / 2;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-//         if (arr[mid] == target) {
-//             ans = mid;
-//             high = mid - 1;  
-//         } 
-//         else if (arr[mid] < target) {
-//             low = mid + 1;
-//         } 
-//         else {
-//             high = mid - 1;
-//         }
-//     }
-//     return ans;
-// }
+        if (arr[mid] == target) {
+            ans = mid;
+            high = mid - 1;  
+        } 
+        else if (arr[mid] < target) {
+            low = mid + 1;
+        } 
+        else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
 
-// int lastOccurrence(vector<int>& arr, int target) {
-//     int low = 0, high = arr.size() - 1;
-//     int ans = -1;
+int lastOccurrence(vector<int>& arr, int target) {
+    int low = 0, high = arr.size() - 1;
+    int ans = -1;
 
-//     while (low <= high) {
-//         int mid = low + (high - low) / 2;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-//         if (arr[mid] == target) {
-//             ans = mid;
-//             low = mid + 1;  
-//         } 
-//         else if (arr[mid] < target) {
-//             low = mid + 1;
-//         } 
-//         else {
-//             high = mid - 1;
-//         }
-//     }
-//     return ans;
-// }
+        if (arr[mid] == target) {
+            ans = mid;
+            low = mid + 1;  
+        } 
+        else if (arr[mid] < target) {
+            low = mid + 1;
+        } 
+        else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
 
-// int countOccurrences(vector<int>& arr, int target) {
-//     int first = firstOccurrence(arr, target);
-//     if (first == -1) return 0;
+int countOccurrences(vector<int>& arr, int target) {
+    int first = firstOccurrence(arr, target);
+    if (first == -1) return 0;
 
-//     int last = lastOccurrence(arr, target);
-//     return last - first + 1;
-// }
+    int last = lastOccurrence(arr, target);
+    return last - first + 1;
+}
