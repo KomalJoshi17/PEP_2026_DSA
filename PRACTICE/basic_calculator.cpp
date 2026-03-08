908a6a9773ddc9d69108e99117b03948b9dd0022
Komal Joshi <moudgillkomal6@gmail.com>
2026-03-08 08:51:29 +0530

    save

---
+++ PRACTICE/basic_calculator.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int calculate(string s) {
+        long long result = 0, number = 0;
+        int sign = 1;
+        stack<long long> st;
+
+        for (char ch : s) {
+
+            if (isdigit(ch)) {
+                number = number * 10 + (ch - '0');
+            }
+            else if (ch == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            }
+            else if (ch == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            }
+            else if (ch == '(') {
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                sign = 1;
+            }
+            else if (ch == ')') {
+                result += sign * number;
+                number = 0;
+
+                result *= st.top(); st.pop();
+                result += st.top(); st.pop();
+            }
+        }
+
+        return result + sign * number;
+    }
+};
