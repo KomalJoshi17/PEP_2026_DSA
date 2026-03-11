8a7bdaff5e519fad59ac14879022f1af2c32fc85
Komal Joshi <moudgillkomal6@gmail.com>
2026-03-11 19:15:36 +0530

    save

---
+++ DEVOPS/deseralize_serialize.cpp
@@ -0,0 +1,42 @@
+class Codec {
+public:
+    void serializeHelper(TreeNode* root, string& result) {
+        if (!root) {
+            result += "null,";
+            return;
+        }
+        result += to_string(root->val) + ",";
+        serializeHelper(root->left, result);
+        serializeHelper(root->right, result);
+    }
+
+    string serialize(TreeNode* root) {
+        string result;
+        serializeHelper(root, result);
+        return result;
+    }
+
+    TreeNode* deserializeHelper(queue<string>& nodes) {
+        string val = nodes.front();
+        nodes.pop();
+
+        if (val == "null") return nullptr;
+
+        TreeNode* root = new TreeNode(stoi(val));
+        root->left = deserializeHelper(nodes);
+        root->right = deserializeHelper(nodes);
+        return root;
+    }
+
+    TreeNode* deserialize(string data) {
+        stringstream ss(data);
+        string token;
+        queue<string> nodes;
+
+        while (getline(ss, token, ',')) {
+            nodes.push(token);
+        }
+
+        return deserializeHelper(nodes);
+    }
+};
