0bb9d85408f4ba4906e2740eca4c30e94370e0bc
KomalJoshi17 <145208745+KomalJoshi17@users.noreply.github.com>
2026-03-13 22:45:14 +0530

    Implement serialize and deserialize methods for Codec

--- DEVOPS/deseralize_serialize.cpp
+++ DEVOPS/deseralize_serialize.cpp
@@ -1,44 +1,44 @@
-// https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
+https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
 
-// class Codec {
-// public:
-//     void serializeHelper(TreeNode* root, string& result) {
-//         if (!root) {
-//             result += "null,";
-//             return;
-//         }
-//         result += to_string(root->val) + ",";
-//         serializeHelper(root->left, result);
-//         serializeHelper(root->right, result);
-//     }
+class Codec {
+public:
+    void serializeHelper(TreeNode* root, string& result) {
+        if (!root) {
+            result += "null,";
+            return;
+        }
+        result += to_string(root->val) + ",";
+        serializeHelper(root->left, result);
+        serializeHelper(root->right, result);
+    }
 
-//     string serialize(TreeNode* root) {
-//         string result;
-//         serializeHelper(root, result);
-//         return result;
-//     }
+    string serialize(TreeNode* root) {
+        string result;
+        serializeHelper(root, result);
+        return result;
+    }
 
-//     TreeNode* deserializeHelper(queue<string>& nodes) {
-//         string val = nodes.front();
-//         nodes.pop();
+    TreeNode* deserializeHelper(queue<string>& nodes) {
+        string val = nodes.front();
+        nodes.pop();
 
-//         if (val == "null") return nullptr;
+        if (val == "null") return nullptr;
 
-//         TreeNode* root = new TreeNode(stoi(val));
-//         root->left = deserializeHelper(nodes);
-//         root->right = deserializeHelper(nodes);
-//         return root;
-//     }
+        TreeNode* root = new TreeNode(stoi(val));
+        root->left = deserializeHelper(nodes);
+        root->right = deserializeHelper(nodes);
+        return root;
+    }
 
-//     TreeNode* deserialize(string data) {
-//         stringstream ss(data);
-//         string token;
-//         queue<string> nodes;
+    TreeNode* deserialize(string data) {
+        stringstream ss(data);
+        string token;
+        queue<string> nodes;
 
-//         while (getline(ss, token, ',')) {
-//             nodes.push(token);
-//         }
+        while (getline(ss, token, ',')) {
+            nodes.push(token);
+        }
 
-//         return deserializeHelper(nodes);
-//     }
-// };
+        return deserializeHelper(nodes);
+    }
+};
